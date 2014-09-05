4f0a4d54d44507f6a68023eb092da5c451949756
unknown <brunoloureirocoelho@hotmail.com>
2014-09-05 08:44:09 -0300

    Projeto vazio

---
+++ CORE/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CORE::Application.config.secret_key_base = 'dd00e7f08e8350afaa41837ee11b53a49e810babaa4311792a106242712132831a574932a2d9372687ae411f54792d5c04884ea862770bd7c72d532b72042187'
