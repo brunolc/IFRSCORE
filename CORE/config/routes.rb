94d4ff766f8b059f816c7e2dc37ea4d750757240
francisco212 <francisco_neto_96@hotmail.com>
2014-10-31 02:36:12 -0200

    controllers criados #6

--- CORE/config/routes.rb
+++ CORE/config/routes.rb
@@ -1,4 +1,11 @@
 CORE::Application.routes.draw do
+
+  resources :alunos
+
+  resources :empresas
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
