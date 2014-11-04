4da957ee5159a1d84335e79feaef561dc8944d3f
francisco212 <francisco_neto_96@hotmail.com>
2014-11-04 03:54:51 -0200

    tudo funcionando menos o excluir que redireciona para o mostra #11

--- CORE/app/controllers/empresas_controller.rb
+++ CORE/app/controllers/empresas_controller.rb
@@ -1,2 +1,43 @@
 class EmpresasController < ApplicationController
+
+	def index
+        @empresas = Empresa.all
+    end
+  def new
+        @empresa = Empresa.new
+    end
+
+    def edit
+        @empresa = Empresa.find(params[:id])
+    end
+
+    def create
+        @empresa = Empresa.new(params.require(:empresa).permit(:nome,:cnpj,:endereco,:cidade,:descricao,:senha))
+        if @empresa.save
+            redirect_to :empresas, notice: "Empresa #{@empresa.nome} salvo"
+        else
+            render :new
+        end
+    end
+
+    def update
+        @empresa = Empresa.find(params[:id])
+        if @empresa.update(params.require(:empresa).permit(:nome,:cnpj,:endereco,:cidade,:descricao,:senha))
+            redirect_to :empresas, notice: "Empresa #{@empresa.nome} atualizado"
+        else
+            render :edit
+        end
+    end
+
+
+    def destroy
+        e = Empresa.find(params[:id])
+        e.destroy
+        redirect_to :empresas, notice: "Empresa #{e.nome} excluido"
+    end
+
+    def show
+        @empresa = Empresa.find(params[:id])
+    end
+
 end
