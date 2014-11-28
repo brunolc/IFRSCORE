fd02d389bece91c1c5f35b01ac94b247b465aca6
Bruno Loureiro <brunoloureirocoelho@hotmail.com>
2014-11-28 13:38:39 -0200

    Issue #29

---
+++ CORE/app/controllers/inscricao_controller.rb
@@ -0,0 +1,26 @@
+class InscricaoController < ApplicationController
+
+
+	def new
+        @vaga = Vaga.new
+    end
+
+	def cadastra
+		if session[:usuario_id].nil?
+			flash[:alert] = 'Voce deve estar logado como usuario para fazer isso'
+        	redirect_to 'root'
+    	else
+    		@inscricao = Inscricao.new
+    		@inscricao.aluno = Aluno.find_by_id(session[:usuario_id])
+       		@inscricao.vaga = Vaga.find_by_id(params[:id])
+        	if @inscricao.save
+        		InscricaoAlunoMailer.realizada(@inscricao).deliver
+        		InscricaoEmpresaMailer.realizada(@inscricao).delivar
+            	redirect_to 'root', notice: "Inscricao realizada"
+        	else
+            	#
+        	end
+        end
+
+	end
+end
