3fef1072e626860750ebee90bc93d02332c88a57
francisco212 <francisco_neto_96@hotmail.com>
2014-11-29 20:53:14 -0200

    senha trocada #59

--- CORE/config/environments/development.rb
+++ CORE/config/environments/development.rb
@@ -22,7 +22,7 @@ CORE::Application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:            'ifrscoretestes',
-    password:             'IFRSCORE123',
+    password:             'TESTESCORE123',
     authentication:       'plain',
     enable_starttls_auto: true  }
 
