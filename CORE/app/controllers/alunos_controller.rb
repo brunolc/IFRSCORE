cd720f858810cd924a29687f9eb9ea8c431be50a
Bruno Loureiro <brunoloureirocoelho@hotmail.com>
2014-12-01 11:32:35 -0200

    Issue #81

--- CORE/app/controllers/alunos_controller.rb
+++ CORE/app/controllers/alunos_controller.rb
@@ -1,44 +1,113 @@
 require 'digest'
 class AlunosController < ApplicationController
     def index
-        @alunos = Aluno.all
-        render layout: 'admin'
+        if !session[:usuario_inativo_id].nil? or !session[:empresa_inativa_id].nil?
+            redirect_to '/ativar' and return
+        end
+        if !session[:usuario_id].nil?
+            flash[:alert] = 'Logado como: aluno ('+session[:usuario]+')'
+            redirect_to '/logado' and return
+        end
+        if !session[:empresa_id].nil?
+            flash[:alert] = 'Logado como: empresa ('+session[:empresa]+')'
+            redirect_to '/logado' and return
+        end
+        if !session[:admin_id].nil?
+            @alunos = Aluno.all
+            render layout: 'admin'
+        end
+            redirect_to 'erro_sessao' and return
     end
    
     def new
-        @aluno = Aluno.new
-        @cursos =Curso.all
+        if !session[:usuario_inativo_id].nil? or !session[:empresa_inativa_id].nil?
+            redirect_to '/ativar' and return
+        end
+        if !session[:usuario_id].nil?
+            flash[:alert] = 'Logado como: aluno ('+session[:usuario]+')'
+            redirect_to '/logado' and return
+        end
+        if !session[:empresa_id].nil?
+            flash[:alert] = 'Logado como: empresa ('+session[:empresa]+')'
+            redirect_to '/logado' and return
+        end
+        if session[:usuario_id].nil?
+            @aluno = Aluno.new
+            @cursos =Curso.all
+            if !session[:admin_id].nil?
+                render layout: 'admin'
+            else
+                render layout: 'aluno'
+            end
+        end
     end
 
     def edit
-         @aluno = Aluno.find(params[:id])
-         @cursos =Curso.all
+        if !session[:usuario_inativo_id].nil? or !session[:empresa_inativa_id].nil?
+            redirect_to '/ativar' and return
+        end
+        if !session[:empresa_id].nil?
+            flash[:alert] = 'Logado como: empresa ('+session[:empresa]+')'
+            redirect_to '/logado' and return
+        end
+        if (!session[:usuario_id].nil? and session[:usuario_id] != params[:id].to_i) or (session[:usuario_id].nil? and session[:admin_id].nil?)
+            redirect_to '/erro_sessao' and return
+        else
+            @aluno = Aluno.find(params[:id].to_i)
+            @cursos =Curso.all
+            if !session[:admin_id].nil?
+                render layout: 'admin'
+            else
+                render layout: 'aluno'
+            end
+        end
     end
 
 
     def create
+        if !session[:usuario_inativo_id].nil? or !session[:empresa_inativa_id].nil?
+            redirect_to '/ativar' and return
+        end
+        if !session[:empresa_id].nil?
+            flash[:alert] = 'Logado como: empresa ('+session[:empresa]+')'
+            redirect_to '/logado' and return
+        end
+        if !session[:usuario_id].nil?
+            flash[:alert] = 'Logado como: aluno ('+session[:usuario]+')'
+            redirect_to '/logado' and return
+        end
         @aluno = Aluno.new(params.require(:aluno).permit(:matricula,:nome,:email,:senha))
         @aluno.senha = Digest::MD5.hexdigest(@aluno.senha)  
         @aluno.ativo = false  
         @aluno.senha_ativacao = SecureRandom.uuid
-         @aluno.curso=Curso.find_by_nome(params[:curso])
+        @aluno.curso=Curso.find_by_nome(params[:curso])
         if @aluno.save
             InscricaoAlunoMailer.cadastro(@aluno).deliver
             @curriculo = Curriculo.new
             @curriculo.aluno = Aluno.find(@aluno)
             @curriculo.save(:validate => false)
-            redirect_to :alunos, notice: "Aluno #{@aluno.nome} salvo"
+            redirect_to :alunos, notice: "Aluno #{@aluno.nome} salvo" and return
         else
             render :new
         end
     end
 
     def update
-        @aluno = Aluno.find(params[:id])
+        if !session[:usuario_inativo_id].nil? or !session[:empresa_inativa_id].nil?
+            redirect_to '/ativar' and return
+        end
+        if !session[:empresa_id].nil?
+            flash[:alert] = 'Logado como: empresa ('+session[:empresa]+')'
+            redirect_to '/logado' and return
+        end
+        if (!session[:usuario_id].nil? and session[:usuario_id] != params[:id].to_i) or (session[:usuario_id].nil? and session[:admin_id].nil?)
+            redirect_to '/erro_sessao' and return
+        end
+        @aluno = Aluno.find(params[:id].to_i)
         @aluno.senha = Digest::MD5.hexdigest(params[:aluno][:senha])  
         @aluno.curso=Curso.find_by_nome(params[:curso])
         if @aluno.update(params.require(:aluno).permit(:matricula,:nome,:email))
-            redirect_to :alunos, notice: "Aluno #{@aluno.nome} atualizado"
+            redirect_to :alunos, notice: "Aluno #{@aluno.nome} atualizado" and return
         else
             render :edit
         end
@@ -46,13 +115,42 @@ class AlunosController < ApplicationController
 
 
     def destroy
-        a = Aluno.find(params[:id])
-        a.destroy
-        redirect_to :alunos, notice: "Aluno #{a.nome} excluido"
+        if !session[:usuario_inativo_id].nil? or !session[:empresa_inativa_id].nil?
+            redirect_to '/ativar' and return
+        end
+        if !session[:admin_id].nil?
+            a = Aluno.find(params[:id].to_i)
+            a.destroy
+            redirect_to :alunos, notice: "Aluno #{a.nome} excluido" and return
+        else 
+            redirect_to 'erro_sessao' and return
+        end
     end
 
     def show
-        @aluno = Aluno.find(params[:id])
+        if !session[:usuario_inativo_id].nil? or !session[:empresa_inativa_id].nil?
+            redirect_to '/ativar' and return
+        end
+        if !session[:empresa_id].nil?
+            flash[:alert] = 'Logado como: empresa ('+session[:empresa]+')'
+            redirect_to '/logado' and return
+        end
+        if (!session[:usuario_id].nil? and session[:usuario_id] != params[:id].to_i) or (session[:usuario_id].nil? and session[:admin_id].nil?)
+=begin      puts "check 1" if (!session[:usuario_id].nil? and session[:usuario_id] != params[:id].to_i)
+            puts "check 2" if (session[:usuario_id].nil? and session[:admin_id].nil?)
+            puts session[:usuario_id].class
+            integ = session[:usuario_id]
+            puts integ.to_s.nil? +'...'
+            puts params[:id].to_i.class
+=end
+            redirect_to '/erro_sessao' and return
+        end
+        @aluno = Aluno.find(params[:id].to_i)
         @cursos=Curso.find(@aluno.curso)
+        if !session[:admin_id].nil?
+            render layout: 'admin'
+        else
+            render layout: 'aluno'
+        end
     end  
 end
--- CORE/app/controllers/curriculos_controller.rb
+++ CORE/app/controllers/curriculos_controller.rb
@@ -9,7 +9,7 @@ class CurriculosController < ApplicationController
 
     def edit_curriculo
     	if session[:usuario_id].nil?
-            redirect_to '/erro_sessao'
+            redirect_to '/erro_sessao' and return
         else
     		@c = Curriculo.find_by_aluno_id(session[:usuario_id])
     		@c.aluno = Aluno.find(session[:usuario_id])
@@ -18,27 +18,36 @@ class CurriculosController < ApplicationController
     		if @c.update(params.require(:curriculo).permit(:nome,:estado_civil,:endereco,:bairro,:cidade,:cep,:uf,:telefone,:celular,:rg,:cpf,:cnh,:formacao_tecnica_curso,:formacao_tecnica_periodo,:formacao_tecnica_entidade,:formacao_tecnica_cidade,:formacao_tecnica_uf,:formacao_superior_curso,:formacao_superior_periodo,:formacao_superior_entidade,:formacao_superior_cidade,:formacao_superior_uf,:formacao_outra_curso,:formacao_outra_periodo,:formacao_outra_entidade,:formacao_outra_cidade,:formacao_outra_uf,:experiencia_empresa_1,:experiencia_funcao_1,:experiencia_inicio_1,:experiencia_fim_1,:experiencia_empresa_2,:experiencia_funcao_2,:experiencia_inicio_2,:experiencia_fim_2,:experiencia_empresa_3,:experiencia_funcao_3,:experiencia_inicio_3,:experiencia_fim_3,:experiencia_empresa_4,:experiencia_funcao_4,:experiencia_inicio_4,:experiencia_fim_4,:experiencia_empresa_5,:experiencia_funcao_5,:experiencia_inicio_5,:experiencia_fim_5,:cursos))
     			redirect_to :alunos, notice: "Curriculo atualizado"
     		else
-    			redirect_to '/perfil/curriculo'
+    			redirect_to '/perfil/curriculo' and return
     		end
     	end
     end
 
     def mostrar
-        respond_to do |format|
-            format.html do
-                redirect_to '/curriculo/mostrar/' + params[:id] + '.pdf'
-                #@curriculo = Curriculo.find_by_aluno_id(params[:id])
-            end
-            format.pdf do
-                if (@curriculo = Curriculo.find_by_aluno_id(params[:id]))
-                 #   string = "Currículo de " + @curriculo.nome
-                    render :pdf => "Curriculo", header: {center: "CURRICULO"}, 
-                    footer: { center: "[page] of [topage]"}
-                else
-                    redirect_to '/erro_404'
+        if !session[:usuario_inativo_id].nil? or !session[:empresa_inativa_id].nil?
+            redirect_to '/ativar' and return
+        end
+        if !session[:empresa_id].nil? or !session[:admin_id].nil? or session[:usuario_id] == params[:id]
+            respond_to do |format|
+                format.html do
+                    redirect_to '/curriculo/mostrar/' + params[:id] + '.pdf' and return
+                    #@curriculo = Curriculo.find_by_aluno_id(params[:id])
+                end
+                format.pdf do
+                    @curriculo = Curriculo.find_by_aluno_id(params[:id])
+                    if !@curriculo.nil?
+                     #   string = "Currículo de " + @curriculo.nome
+                        render :pdf => "Curriculo", header: {center: "CURRICULO"}, 
+                        footer: { center: "[page] of [topage]"}
+                    else
+                        redirect_to '/erro_404' and return
+                    end
                 end
             end
+        else
+            redirect_to '/erro_sessao' and return
         end
+
     end
         
 end
